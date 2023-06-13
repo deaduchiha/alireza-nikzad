d21cab32bcd02cb90b5ae9240ac22942392029e7
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-13 13:18:36 +0330

    add email section

--- components/elements/first-page/first-page.tsx
+++ components/elements/first-page/first-page.tsx
@@ -1,9 +1,31 @@
-"use client";
 /* eslint-disable react/no-unescaped-entities */
-import styles from "./first-page.module.css";
+"use client";
 
+import styles from "./first-page.module.css";
 import Typewriter from "typewriter-effect";
 import Slider from "./parts/slider";
+
+import { GrLinkedin } from "react-icons/gr";
+import { ImGithub } from "react-icons/im";
+import { GrInstagram } from "react-icons/gr";
+
+const data = [
+  {
+    name: "linkedin",
+    icon: <GrLinkedin />,
+    url: "https://www.linkedin.com/in/alireza-nikzad/",
+  },
+  {
+    name: "github",
+    icon: <ImGithub />,
+    url: "https://github.com/deaduchiha",
+  },
+  {
+    name: "instagram",
+    icon: <GrInstagram />,
+    url: "https://www.instagram.com/alirezankz/",
+  },
+];
 
 const FirstPage = () => {
   return (
@@ -29,6 +51,19 @@ const FirstPage = () => {
         <div className={styles.photo}>
           <Slider />
         </div>
+      </div>
+
+      <div className={`${styles["social-media"]}`}>
+        <div className={styles.icons}>
+          {data.map((social) => (
+            <a key={social.name} href={social.url} target="_blank">
+              {social.icon}
+            </a>
+          ))}
+        </div>
+        <a className={styles.email} href="mailto:example@example.com">
+          alireza.nikzdd@gmail.com
+        </a>
       </div>
     </div>
   );
