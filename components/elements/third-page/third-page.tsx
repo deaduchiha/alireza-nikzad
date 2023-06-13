d224246aa83d1bd765875b19057d8440d38e725c
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-13 16:57:54 +0330

    create third page and clean extra codes

---
+++ components/elements/third-page/third-page.tsx
@@ -0,0 +1,11 @@
+import styles from "./third-page.module.css";
+
+const ThirdPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Experience</h1>
+    </div>
+  );
+};
+
+export default ThirdPage;
--- components/templates/landing.tsx
+++ components/templates/landing.tsx
@@ -1,13 +1,13 @@
-"use client";
-
 import FirstPage from "../elements/first-page/first-page";
 import SecondPage from "../elements/second-page/second-page";
+import ThirdPage from "../elements/third-page/third-page";
 
 const Landing = () => {
   return (
     <>
       <FirstPage />
       <SecondPage />
+      <ThirdPage />
     </>
   );
 };
