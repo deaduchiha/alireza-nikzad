2625ab3426c8cf8f61523b03bb8258ab1adb06ef
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-13 12:53:17 +0330

    complete biography section

--- components/elements/first-page/first-page.tsx
+++ components/elements/first-page/first-page.tsx
@@ -8,22 +8,27 @@ import Slider from "./parts/slider";
 const FirstPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.biography}>
-        <p>
-          I'm <b>Alireza Nikzad</b>
-        </p>
+      <div className={styles.biography__container}>
+        <div className={styles.biography}>
+          <p>
+            I'm <b>Alireza Nikzad</b>
+          </p>
 
-        <Typewriter
-          component="p"
-          options={{
-            strings: ["Front-end-developer, UI & UX designer and student"],
-            autoStart: true,
-            loop: true,
-          }}
-        />
-      </div>
-      <div className={styles.photo}>
-        <Slider />
+          <Typewriter
+            component="p"
+            options={{
+              strings: [
+                "Experienced in creating visually appealing interfaces as a Front-end developer and UI/UX design enthusiast, with a medium foundation in backend development.",
+              ],
+              autoStart: true,
+              loop: true,
+              delay: 50,
+            }}
+          />
+        </div>
+        <div className={styles.photo}>
+          <Slider />
+        </div>
       </div>
     </div>
   );
