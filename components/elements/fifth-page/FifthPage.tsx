0c3fb1fd73e1a1cdce5a7d984c4b38fb89d872d4
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-09-20 21:54:00 +0330

    update portfolio

--- components/elements/fifth-page/FifthPage.tsx
+++ components/elements/fifth-page/FifthPage.tsx
@@ -1,8 +1,21 @@
 import styles from "./fifth-page.module.css";
+import { data } from "./data";
+
 const FifthPage = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Projects</h2>
+      {data.map((data) => (
+        <div key={data.id} className={styles.projectHolder}>
+          <h3>{data.project}</h3>
+          <p>{data.description}</p>
+          <div className={styles.tools}>
+            {data.tools.map((tool, index) => (
+              <span key={index}>{tool}</span>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ components/elements/fifth-page/data.ts
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 1,
+    project: "HKA web application",
+    description:
+      "Collaboration with the knowledge-based company HKA and implementing a website\
+    for them to showcase completed projects and available products.",
+    tools: [
+      "NextJS 13.4",
+      "Chakraui",
+      "Swiper",
+      "Typescript",
+      "React icons",
+      "Figma",
+    ],
+  },
+];
