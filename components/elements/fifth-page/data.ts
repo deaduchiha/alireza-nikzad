55c56c21ad7cf08eecc92e582233a727ffcc6b89
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-11-22 03:16:57 +0330

    update data

--- components/elements/fifth-page/data.ts
+++ components/elements/fifth-page/data.ts
@@ -21,4 +21,27 @@ export const data = [
       "Implementation of various restaurant and cafe menus for the Rhino team.",
     tools: ["Vanilla JavaScript", "Jquery", "Swiper", "Svg external loader"],
   },
+  {
+    id: 3,
+    project: "Aryana Academy",
+    description:
+      "Complete website design with expertise in both frontend and backend for the language academy Ariana, aimed at selling language courses and managing students' knowledge in-person.",
+    tools: [
+      "NextJS 13.4",
+      "Chakraui",
+      "Swiper",
+      "Typescript",
+      "React icons",
+      "Figma",
+      "Zod",
+      "React hook form",
+      "parallel routes",
+      "kavenegar sms panel",
+      "axios",
+      "React query",
+      "loadsh",
+      "next auth",
+      "zustand",
+    ],
+  },
 ];
